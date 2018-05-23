44d1d9fa202edf486b102be16a16111561fa9c51
NagaTheDon <nagaarjun09@gmail.com>
2018-05-23 21:56:30 +0100

    Removed the comments. Need to add ID input code

--- CodeJams/pancakes.py
+++ CodeJams/pancakes.py
@@ -1,22 +1,11 @@
 input = list("---------- ")
-
-#print(input)
+=
 flips_allowed = 3
 
 count = 0
 
 input_string = "".join(input)
-
-# Check how many minues there are and collect their indices 
-
-# At each minus, flip the next three flips 
-
-# Repeat it until the minus are gone 
-
-# If one of the pattern is the same as before, break the while loop and say it is impossible 
-# print(input.count("-"))
-# plus_index = input.index("-")
-# print(plus_index)
+=
 
 completed = False;
 
@@ -31,7 +20,6 @@ while (input_string != "".join(["+" for j in range(len(input_string))])):
 	else:
 		break
 
-	# print("after count",count)
 	print(input)
 	for j in range(flips_allowed):
 		if(plus_index + j < len(input)):
@@ -51,5 +39,3 @@ if(input_string == "".join(["+" for j in range(len(input_string))])):
 else:
 	print("IMPOSSIBLE!")
 
-
-#print(input)
